3f4c23275661d7e0a71ee35f19fc706989476447
Mason <mason.naka@gmail.com>
2026-01-03 21:44:52 +0000

    removed negotiation -- Deprecated

--- envs/negotiation/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Negotiation environments module - contains trading environment."""
-from .trading import TradingGameEnvironment
-
-__all__ = ['TradingGameEnvironment']
--- envs/negotiation/trading/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Trading environment module."""
-from .trading_env import TradingGameEnvironment
-
-__all__ = ['TradingGameEnvironment']
